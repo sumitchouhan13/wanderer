827ffa61b2436eac1a882a78a224a30a94691e58
Sumit Chouhan <sumitchouhan8933@gmail.com>
2023-04-07 19:00:18 +0530

    4th April 2023

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,6 @@ function Contact() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        backgroundColor: "#E2BEF1",
       }}
     >
       Contact Page
