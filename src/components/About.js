9b92e170da12e7848986e60cb6a9ae2248f613f2
Sumit Chouhan <sumitchouhan8933@gmail.com>
2023-04-16 15:10:57 +0530

    14April2023

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { useRef, useEffect, useState } from "react";
 import { BiLike, BiDislike } from "react-icons/bi";
-import image1 from "../images/pexels-bhawani-shankar-kumawat-4023930.jpg";
-import image2 from "../images/pexels-bhumik-photography-5621421.jpg";
-import image3 from "../images/pexels-parv-choudhary-2870167.jpg";
-import profile from "../images/random_people.jpg";
+import aboutData from "../data/aboutData";
 
 function About() {
   const [width, setWidth] = useState(0);
@@ -27,230 +24,67 @@ function About() {
             dragConstraints={{ right: 0, left: -width }}
             className="inner-carousel"
           >
-            <motion.div className="item">
-              <img src={image1} alt="travelImage" className="item-img-tag" />
-              <div className="comments-card">
-                <div className="card-image-container">
-                  <div className="card-image-inner-container">
-                    <img
-                      src={profile}
-                      alt="profile"
-                      className="card-image-tag"
-                    />
-                  </div>
-                </div>
-                <div style={{ width: "70%" }}>
-                  <div style={{ height: "65%" }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    .
-                  </div>
-                  <div style={{ height: "35%", display: "flex" }}>
-                    <div style={{ width: "50%", display: "flex" }}>
-                      <BiLike
-                        size={25}
-                        color="inherit"
-                        style={{ width: "20%" }}
-                      />
-                      <div
-                        style={{
-                          width: "80%",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "10%",
-                        }}
-                      >
-                        234
+            {aboutData.map(
+              ({ id, imageUrl, personImage, likes, dislikes, content }) => (
+                <motion.div className="item" key={id}>
+                  <img
+                    src={imageUrl}
+                    alt="travelImage"
+                    className="item-img-tag"
+                  />
+                  <div className="comments-card">
+                    <div className="card-image-container">
+                      <div className="card-image-inner-container">
+                        <img
+                          src={personImage}
+                          alt="profile"
+                          className="card-image-tag"
+                        />
                       </div>
                     </div>
-                    <div style={{ width: "50%", display: "flex" }}>
-                      <BiDislike
-                        size={25}
-                        color="inherit"
-                        style={{ width: "20%" }}
-                      />
-                      <div
-                        style={{
-                          width: "80%",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "10%",
-                        }}
-                      >
-                        123
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </motion.div>
-            <motion.div className="item">
-              <img src={image2} alt="travelImage" className="item-img-tag" />
-              <div className="comments-card">
-                <div className="card-image-container">
-                  <div className="card-image-inner-container">
-                    <img
-                      src={profile}
-                      alt="profile"
-                      className="card-image-tag"
-                    />
-                  </div>
-                </div>
-                <div style={{ width: "70%" }}>
-                  <div style={{ height: "65%" }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    .
-                  </div>
-                  <div style={{ height: "35%", display: "flex" }}>
-                    <div style={{ width: "50%", display: "flex" }}>
-                      <BiLike
-                        size={25}
-                        color="inherit"
-                        style={{ width: "20%" }}
-                      />
-                      <div
-                        style={{
-                          width: "80%",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "10%",
-                        }}
-                      >
-                        234
-                      </div>
-                    </div>
-                    <div style={{ width: "50%", display: "flex" }}>
-                      <BiDislike
-                        size={25}
-                        color="inherit"
-                        style={{ width: "20%" }}
-                      />
-                      <div
-                        style={{
-                          width: "80%",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "10%",
-                        }}
-                      >
-                        123
+                    <div style={{ width: "70%" }}>
+                      <div style={{ height: "65%" }}>{content}</div>
+                      <div style={{ height: "35%", display: "flex" }}>
+                        <div style={{ width: "50%", display: "flex" }}>
+                          <BiLike
+                            size={25}
+                            color="inherit"
+                            style={{ width: "20%" }}
+                          />
+                          <div
+                            style={{
+                              width: "80%",
+                              display: "flex",
+                              alignItems: "center",
+                              paddingLeft: "10%",
+                            }}
+                          >
+                            {likes}
+                          </div>
+                        </div>
+                        <div style={{ width: "50%", display: "flex" }}>
+                          <BiDislike
+                            size={25}
+                            color="inherit"
+                            style={{ width: "20%" }}
+                          />
+                          <div
+                            style={{
+                              width: "80%",
+                              display: "flex",
+                              alignItems: "center",
+                              paddingLeft: "10%",
+                            }}
+                          >
+                            {dislikes}
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </motion.div>
-            <motion.div className="item">
-              <img src={image3} alt="travelImage" className="item-img-tag" />
-              <div className="comments-card">
-                <div className="card-image-container">
-                  <div className="card-image-inner-container">
-                    <img
-                      src={profile}
-                      alt="profile"
-                      className="card-image-tag"
-                    />
-                  </div>
-                </div>
-                <div style={{ width: "70%" }}>
-                  <div style={{ height: "65%" }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    .
-                  </div>
-                  <div style={{ height: "35%", display: "flex" }}>
-                    <div style={{ width: "50%", display: "flex" }}>
-                      <BiLike
-                        size={25}
-                        color="inherit"
-                        style={{ width: "20%" }}
-                      />
-                      <div
-                        style={{
-                          width: "80%",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "10%",
-                        }}
-                      >
-                        234
-                      </div>
-                    </div>
-                    <div style={{ width: "50%", display: "flex" }}>
-                      <BiDislike
-                        size={25}
-                        color="inherit"
-                        style={{ width: "20%" }}
-                      />
-                      <div
-                        style={{
-                          width: "80%",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "10%",
-                        }}
-                      >
-                        123
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </motion.div>
-            <motion.div className="item">
-              <img src={image1} alt="travelImage" className="item-img-tag" />
-              <div className="comments-card">
-                <div className="card-image-container">
-                  <div className="card-image-inner-container">
-                    <img
-                      src={profile}
-                      alt="profile"
-                      className="card-image-tag"
-                    />
-                  </div>
-                </div>
-                <div style={{ width: "70%" }}>
-                  <div style={{ height: "65%" }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    .
-                  </div>
-                  <div style={{ height: "35%", display: "flex" }}>
-                    <div style={{ width: "50%", display: "flex" }}>
-                      <BiLike
-                        size={25}
-                        color="inherit"
-                        style={{ width: "20%" }}
-                      />
-                      <div
-                        style={{
-                          width: "80%",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "10%",
-                        }}
-                      >
-                        234
-                      </div>
-                    </div>
-                    <div style={{ width: "50%", display: "flex" }}>
-                      <BiDislike
-                        size={25}
-                        color="inherit"
-                        style={{ width: "20%" }}
-                      />
-                      <div
-                        style={{
-                          width: "80%",
-                          display: "flex",
-                          alignItems: "center",
-                          paddingLeft: "10%",
-                        }}
-                      >
-                        123
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </motion.div>
+                </motion.div>
+              )
+            )}
           </motion.div>
         </motion.div>
       </div>
---
+++ src/data/itineraryData.js
@@ -0,0 +1,53 @@
+import JaipurImage from "../images/jaipur_image.jpg";
+import UdaipurImage from "../images/udaipur_image.jpg";
+import JaisalmerImage from "../images/jaisalmer_image.jpg";
+import DesertLifeImage from "../images/desert-life-image.jpg";
+import RajasthanFortImage from "../images/rajasthan-forts-image.jpg";
+import RajathanCuisineImage from "../images/rajasthan-cuisine-image.jpg";
+
+const itineraryData = [
+  {
+    id: 1,
+    imageUrl: JaipurImage,
+    heading: "Jaipur",
+    content:
+      "Jaipur is the capital and the largest city of the Indian state of Rajasthan.",
+  },
+  {
+    id: 2,
+    imageUrl: UdaipurImage,
+    heading: "Udaipur",
+    content:
+      "Udaipur, also known as the City of Lakes, is a city in the state of Rajasthan in western India.",
+  },
+  {
+    id: 3,
+    imageUrl: JaisalmerImage,
+    heading: "Jaisalmer",
+    content:
+      "Jaisalmer is a city in the Indian state of Rajasthan. It is located in the heart of the Thar Desert.",
+  },
+  {
+    id: 4,
+    imageUrl: DesertLifeImage,
+    heading: "Experience the desert life",
+    content:
+      "Take a camel safari and explore the vast sand dunes of Rajasthan.",
+  },
+  {
+    id: 5,
+    imageUrl: RajasthanFortImage,
+    heading: "Visit the royal fort",
+    content:
+      "Explore the grandeur of the majestic forts and palaces built by the Rajput rulers.",
+  },
+  {
+    id: 6,
+    imageUrl: RajathanCuisineImage,
+    heading: "Taste the local cuisine",
+    content:
+      "Indulge in the rich and diverse cuisine of Rajasthan, from dal bati churma to laal maas.",
+  },
+];
+
+export default itineraryData;
